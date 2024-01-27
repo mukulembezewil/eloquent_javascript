a86bc272a5f0980d432381e404573e4d564d4585
mukulembezewil <wilmukulembeze@gmail.com>
2024-01-27 12:04:28 +0300

    change file extension to .mjs for module's sake

--- 5_Higher_Order_Functions/4_filtering_arrays.mjs
+++ 5_Higher_Order_Functions/4_filtering_arrays.mjs
@@ -1,5 +1,6 @@
-import { SCRIPTS } from './3_scripts.mjs';
+import { SCRIPTS } from './3_scripts.mjs'; // This line imports a variable called 'SCRIPTS' from a module located at the relative path './3_scripts.mjs'. A module is a way to organize and encapsulate JavaScript code.
 
+// Defining the 'filter function
 function filter(array, test) {
 	let passed = [];
 	for (let element of array) {
@@ -11,6 +12,14 @@ function filter(array, test) {
 }
 console.log(filter(SCRIPTS, (script) => script.living));
 
+/* Defining the filter function
+The above function named 'filter', takes two parameters: 'array' (an array) and 'test' (a function). It iterates through each element in the 'array' and applies the 'test' function to each element. If the 'test' functions returns 'true' for a given element, that element is added to a new array called 'passed'. Finally, the 'passed' array is returned.
+
+Calling 'filter' on 'SCRIPTS':
+On line 13 the 'filter' function is called with 'SCRIPTS' as the array and an arrow function '(script) => script.living' as the test function. This arrow function checks the 'living' property of each 'script' object. If 'script.living' is truthy, it means the script is associated with a living language, and the script is included in the result.
+The filtered result is then logged to the console using 'console.log'.
+
+In summary the code filters the 'SCRIPTS' array, selecting only those objects where the 'living' property is truthy, and logs the result to the console. The 'filter' function is a generic utility function that can be reused for different filtering criteria.
 /*
 NOTE: Use of ES module loading.
 (node:94110) Warning: To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
